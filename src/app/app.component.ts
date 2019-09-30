5ab9d1939e54514cfc26226c287c1c1256b39219
David Rivas <david.rivas@pevaar.com>
2019-09-30 16:27:16 -0500

    I will close this editor lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @Input() artist: string;
-  @Input() song: string;
-  readonly title: string = 'Api Lyrics Ng';
-  readonly url: string = 'https://api.lyrics.ovh/v1/'; // https://api.lyrics.ovh/v1/artist/title
-  lyrics: string;
+    @Input() artist: string;
+    @Input() song: string;
+    readonly title: string = 'Api Lyrics Ng';
+    readonly url: string = 'https://api.lyrics.ovh/v1/'; // https://api.lyrics.ovh/v1/artist/title
+    lyrics: string;
 
-  searchForASong(): void {
-    if (this.artist == null || this.song == null
-      || this.artist.length === 0 || this.artist.length === 0) {
-        let request: string = `${this.url}/${this.artist}/${this.song}`;
-      }
-  }
+    searchForASong(): void {
+        if (this.artist == null || this.song == null ||
+            this.artist.length === 0 || this.artist.length === 0) {
+            let request: string = `${this.url}/${this.artist}/${this.song}`;
+        }
+    }
 }
