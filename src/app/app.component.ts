f390b949ac359d243d0796b3dfa2eae5f47a40a0
David Rivas <david.rivas@pevaar.com>
2019-10-22 10:28:01 -0500

    Changes from october 22nd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,39 @@ import { Component, Input } from '@angular/core';
 import { LyricsService } from './lyrics.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  readonly title: string = 'Api Lyrics Ng';
+    readonly title: string = 'Api Lyrics Ng';
 
-  @Input() artist: string;
-  @Input() song: string;
-  lyrics: string;
+    @Input() artist: string;
+    @Input() song: string;
+    lyrics: string;
+    isBusy: boolean;
 
-  constructor(private lyricsService: LyricsService) { }
-
-  searchForASong(): void {
-    if (!this.isNullOrWhitespace(this.artist) || !this.isNullOrWhitespace(this.song)) {
-      this.lyricsService.getLyrics(this.artist, this.song).subscribe(
-        result => this.lyrics = result.lyrics,
-        error =>  {
-          this.lyrics = null;
-          alert(error.message);
-        }
-      );
-    } else {
-      alert('You must provide an artist or song');
+    constructor(private lyricsService: LyricsService) {
+        this.isBusy = false;
     }
-  }
 
-  private isNullOrWhitespace(str: string): boolean {
-    return str == null || str.trim().length === 0 || str === undefined;
-  }
+    searchForASong(): void {
+        if (!this.isNullWhitespaceOrUndefined(this.artist) || !this.isNullWhitespaceOrUndefined(this.song)) {
+            this.isBusy = true;
+            this.lyricsService.getLyrics(this.artist, this.song).subscribe(
+                result => this.lyrics = result.lyrics,
+                error => {
+                    this.lyrics = null;
+                    alert(error.message);
+                }
+            );
+            this.isBusy = false;
+        } else {
+            alert('You must provide an artist or song');
+        }
+    }
+
+    private isNullWhitespaceOrUndefined(str: string): boolean {
+        return str == null || str.trim().length === 0 || str === undefined;
+    }
 }
